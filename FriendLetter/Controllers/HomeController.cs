d80767e563d36cb08d0f3165b0c4ec8a31cd5e41
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-17 15:24:06 -0700

    add views

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -4,8 +4,11 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
+    [Route("/goodbye")]
     public string Goodbye() { return "Byebye pal!"; }
-
+    [Route("/")]
+    public ActionResult Letter() {return View();}
   }
 }
