04c8d222be9e5389a90f0de3402b7d81f45b63c2
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-17 16:53:19 -0700

    add form fields and dynamic view

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -15,5 +15,15 @@ namespace FriendLetter.Controllers
       myLetterVariable.Recipient = "Lauren";
       myLetterVariable.Sender = "Jessica";
       return View(myLetterVariable);}
+    [Route("/form")]
+    public ActionResult Form() { return View();}
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
